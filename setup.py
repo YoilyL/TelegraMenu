b69a994caf83ae711f8edab5b75faced00026b34
YoilyL <yoilychat@gmail.com>
2019-06-18 02:47:19 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TelegraMenu',
+      version='0.0.1',
+      description='Telegram Menus',
+      url='https://github.com/YoilyL/TelegraMenu',
+      author='Yoily',
+      author_email='yoilyl@telegram.org',
+      license='GPL',
+      packages=['telegramenu'],
+      zip_safe=False)
