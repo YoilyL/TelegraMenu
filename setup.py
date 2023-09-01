b8025edd6ca5822f78e43dd921f35642297dea85
YoilyL <yoilychat@gmail.com>
2023-09-01 17:05:44 -0400

    enforce minimum version because of __init_subclass__

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+import sys
+if sys.version_info < (3,6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+      
 setup(name='TelegraMenu',
       version='0.0.1',
       description='Telegram Menus',
